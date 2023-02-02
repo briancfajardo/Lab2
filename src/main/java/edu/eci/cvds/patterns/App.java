08462cc32726b46d41e5760d8bca5f95f1f5341a
unknown <brian.fajardo@labinfo.is.escuelaing.edu.co>
2023-02-02 18:43:06 -0500

    Lab2

---
+++ src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,14 @@
+package edu.eci.cvds.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args)
+    {
+        
+        System.out.println( "Hello " + args[0] + " " + args[1]);
+    }
+}
